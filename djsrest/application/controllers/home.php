6caf5d3b49b4adde4960ed52d8b9f2b0c5eec5f2
Eric Lundy <Eric Lundy>
2016-01-29 05:29:18 -0500

    Adding new files to webiste

---
+++ djsrest/application/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		$data['title'] = 'Home Page';
+		$this->load->view('template/navigation');
+		$this->load->view('template/header', $data);
+		$this->load->view('home');
+		$this->load->view('template/footer');
+	}
+}
---
+++ djsrest/application/views/home.php
@@ -0,0 +1,55 @@
+
+<div class="page-header text-center"><h1>Welcome to DJ's Bistro</h1></div>
+<div class="container">
+	<div id="theCarousel" class="carousel slide" data-ride="carousel">
+	 
+	  <!-- Define how many slides to put in the carousel -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#theCarousel" data-slide-to="0" class="active"> </li >
+	    <li data-target="#theCarousel" data-slide-to="1"> </li>
+	    <li data-target ="#theCarousel" data-slide-to="2"> </li>
+	    <li data-target ="#theCarousel" data-slide-to="3"> </li>
+	    <li data-target ="#theCarousel" data-slide-to="4"> </li>
+	    <li data-target ="#theCarousel" data-slide-to="5"> </li>
+	  </ol >
+	 
+	  <!-- Define the text to place over the image -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+        	<img class="img-responsive center-block" src="/assets/img/outside.jpg" alt="outside" width="482" height="362">
+      	</div>
+
+      	<div class="item">
+        	<img class="img-responsive center-block" src="/assets/img/dinnerslide.jpg" alt="dinnerslide" width="482" height="362">
+      	</div>
+
+      	<div class="item">
+        	<img class="img-responsive center-block" src="/assets/img/barslide.jpg" alt="barslide" width="482" height="362">
+      	</div>
+
+      	<div class="item">
+        	<img class="img-responsive center-block" src="/assets/img/food1.jpg" alt="food1" width="482" height="362">
+      	</div>
+
+      	<div class="item">
+        	<img class="img-responsive center-block" src="/assets/img/food2.jpg" alt="food2" width="482" height="362">
+      	</div>
+
+      	<div class="item">
+        	<img class="img-responsive center-block" src="/assets/img/food3.jpg" alt="food3" width="482" height="362">
+      	</div>
+      	
+	   </div>
+	</div>
+</div>
+
+	 
+	  <!-- Set the actions to take when the arrows are clicked -->
+	  <a class="left carousel-control" href="#theCarousel" data-slide="prev">
+	  <span class="glyphicon glyphicon-chevron-left"> </span>
+	  </a>
+	  <a class="right carousel-control" href="#theCarousel" data-slide="next">
+	  <span class="glyphicon glyphicon-chevron-right"></span>
+	  </a>
+	  </div>
+</div>	
