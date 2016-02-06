5610877ec758b7d4f44d2d01a644cd78ade42849
lehos24 <kulise01@franklin.mail.edu>
2016-02-06 13:58:38 -0500

    Added link to Reservation menu

--- djsrest/application/views/template/navigation.php
+++ djsrest/application/views/template/navigation.php
@@ -50,7 +50,7 @@
 			<div class="col-md-3" id="leftCol">              
 				<ul class="nav nav-stacked" id="sidebar">
 				  <li><a href="#sec0">Home</a></li>
-				  <li><a href="#sec1">Reservations</a></li>
+				  <li><?php echo anchor('reservations', 'Reservations');?></li>
 				  <li><a href="#sec2">Menu</a></li>
 				  <li><a href="#sec3">Contact Us</a></li>
 				</ul>              
