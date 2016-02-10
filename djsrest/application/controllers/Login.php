fef09fa3a6b71eb26aec49b4395988968f7456b8
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-02-09 19:23:28 -0500

    added empty routine stubs for login controller

--- djsrest/application/controllers/Login.php
+++ djsrest/application/controllers/Login.php
@@ -5,10 +5,36 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
+		$this->view_login_page();
+	}
+	
+	/**
+	 * loads the login page
+	 */
+	public function view_login_page() {
 		$data['title'] = 'Login';
 		$this->load->view('template/header', $data);
 		$this->load->view('template/navigation');
 		$this->load->view('login_view');
 		$this->load->view('template/footer');
 	}
+	
+	/**
+	 * this function validates the login data.
+	 *  
+	 * make sure to trim the data
+	 * use md5 for the password field
+	 * use xss_clean in order to prevent cross-site scripting 
+	 */
+	public function login_validation() {
+		
+	}
+	
+	/**
+	 * this function creates a user model and use it to verify the 
+	 * credentials against the database. 
+	 */
+	public function validate_credentials() {
+		
+	}		
 }
