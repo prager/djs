9e76151bfe39a2db162dd53eae93685913b4df0e
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-02-09 19:29:44 -0500

    added empty routine stubs for User_model

--- djsrest/application/models/User_model.php
+++ djsrest/application/models/User_model.php
@@ -5,7 +5,17 @@ class User_model extends CI_Model {
     {
         parent::__construct();
     }
+    
+    /**
+     * check if the user exist in the database and passsword entered 
+     * matches with the password in database.
+     * 
+     * return true if credentials are valid
+     * 
+     */
+    public function can_login() {
+    	
+    }
 
 }
-
 ?>
