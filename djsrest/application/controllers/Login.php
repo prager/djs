e5440f90a1d5a13d6410586eb111d16094689835
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-02-14 13:59:17 -0500

    added functions for the registration feature

--- djsrest/application/controllers/Login.php
+++ djsrest/application/controllers/Login.php
@@ -36,5 +36,54 @@ class Login extends CI_Controller {
 	 */
 	public function validate_credentials() {
 		
-	}		
+	}	
+	
+	//------added by Sajith
+	
+	public function load_registration() {
+		$data['title'] = 'Registration Page';
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/navigation');
+		$this->load->view('registration_view');
+		$this->load->view('templates/footer');
+	}
+	
+	public function load_registration_success() {
+		$data['title'] = 'Success Page';
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/navigation');
+		$this->load->view('success_view');
+		$this->load->view('templates/footer');
+	}
+	
+	public function registration_validation() {
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+	
+		$this->form_validation->set_rules('firstName', 'First Name', 'trim|required|alpha');
+		$this->form_validation->set_rules('lastName', 'Last Name', 'trim|required|alpha');
+		$this->form_validation->set_rules('address1', 'Address line', 'trim|required');
+		$this->form_validation->set_rules('city', 'City', 'trim|required');
+		$this->form_validation->set_rules('state', 'State', 'trim|required');
+		$this->form_validation->set_rules('zip', 'Zip', 'trim|required');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[32]|xss_clean|is_unique[user_tbl.username]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|matches[emailConf]|is_unique[user_tbl.email_addr]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|matches[passconf]|md5');
+		$this->form_validation->set_rules('passConf', 'Password Confirmation', 'trim|required');
+	
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->view_registration_page();
+		}
+		else
+		{
+			$this->create_customer_account();
+			$this->load_registration_success();
+		}
+	}
+	
+	public function create_customer_account() {
+		$this->load->model('User_model', '', TRUE);
+		$this->User_model->create_user('customer');		
+	}
 }
