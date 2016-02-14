a152e991d4b659681fb2d88e666ce9f0ba7b7a97
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-02-14 13:35:01 -0500

    added function to insert a user to the database

--- djsrest/application/models/User_model.php
+++ djsrest/application/models/User_model.php
@@ -16,6 +16,24 @@ class User_model extends CI_Model {
     public function can_login() {
     	
     }
+    
+    //--- added by sajith---
+    
+    public function add_user_account($user_type) {
+    	$data = array(
+    			'username' => $this->input->post('username'),
+    			'first_nm' => md5($this->input->post('firstName')),
+    			'last_nm' => $this->input->post('lastName'),
+    			'email_addr' => $this->input->post('email'),
+    			'addr_line1' => $this->input->post('address1'),
+    			'addr_line2' => $this->input->post('address2'),
+    			'state_cd' => $this->input->post('state'),
+    			'zip_cd' => $this->input->post('zip'),
+    			'user_type' => $user_type
+    	);
+    
+    	return $this->db->insert('user_tbl', $data);
+    }
 
 }
 ?>
