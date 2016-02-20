cde3265adf0fec2b24c6fd34151e2da1c4e6af91
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-02-20 18:42:06 -0500

    added the controller for admin feature

---
+++ djsrest/application/controllers/Admin.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function index()
+	{
+		$this->load_admin_page();
+	}
+	
+	public function load_admin_page() {
+		$data['title'] = 'Administrator';
+		$this->load->view('template/header', $data);
+		$this->load->view('template/navigation');
+		$this->load->view('admin_view');
+		$this->load->view('template/footer');
+	}
+}
