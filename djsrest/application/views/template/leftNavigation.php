055b250fb1d7e9ec26df7b6431bd792c506487aa
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-02-22 20:29:19 -0500

    adjust the spacing

--- djsrest/application/views/template/leftNavigation.php
+++ djsrest/application/views/template/leftNavigation.php
@@ -1,11 +1,11 @@
 <div class="container">
-			<div class="row">
-			<!-- left side navigation bar -->
-			<div class="col-md-3" id="leftCol">              
-				<ul class="nav nav-stacked" id="sidebar">
-				  <li><?php echo anchor('home', 'Home');?></li>
-				  <li><?php echo anchor('reservations', 'Reservations');?></li>
-				  <li><a href="#sec2">Menu</a></li>
-				  <li><a href="#sec3">Contact Us</a></li>
-				</ul>              
-			</div>
+	<div class="row">
+		<!-- left side navigation bar -->
+		<div class="col-md-3" id="leftCol">              
+			<ul class="nav nav-stacked" id="sidebar">
+			  <li><?php echo anchor('home', 'Home');?></li>
+			  <li><?php echo anchor('reservations', 'Reservations');?></li>
+			  <li><a href="#sec2">Menu</a></li>
+			  <li><a href="#sec3">Contact Us</a></li>
+			</ul>              
+		</div>
