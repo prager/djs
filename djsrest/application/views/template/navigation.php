4beba373b4be2c69fc7d79f8c899a279b6e83744
sajth Wanigasinghe <waniga02@email.franklin.edu>
2016-03-01 10:14:36 -0500

    fixed minor spacing issues

--- djsrest/application/views/template/navigation.php
+++ djsrest/application/views/template/navigation.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-bright navbar-fixed-top" role="banner">
-		<div class="container">
+	<div class="container">
 		<div class="navbar-header">
 		  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
 			<span class="sr-only">Toggle navigation</span>
@@ -28,18 +28,18 @@
 				<li>
 			  		<a href="<?php echo base_url() ;?>index.php/login">Login <span class="glyphicon glyphicon-log-in"></span></a>
 				</li>
-		  	</ul>
-
+			</ul>
+			
 		</nav>
-	  </div>
-	</header>
+	</div>
+</header>
 
 <div id="masthead">  
-	  <div class="container">
-		<div class="row">
-			<div class="col-md-7">
-				<img id="brand" class="img-responsive" src="<?php echo base_url() ;?>/assets/img/Brand.png" alt="Logo">
-			</div>
-		</div> 
-	  </div><!-- /cont -->
-	</div>
+  <div class="container">
+	<div class="row">
+		<div class="col-md-7">
+			<img id="brand" class="img-responsive" src="<?php echo base_url() ;?>/assets/img/Brand.png" alt="Logo">
+		</div>
+	</div> 
+  </div><!-- /cont -->
+</div>
